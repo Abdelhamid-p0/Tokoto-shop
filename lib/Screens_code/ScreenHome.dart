a5e2cf61cba382c7a9e7c99fa8c957e7e615f6aa
Abdelhamid-0p <abdelhamid.hachimi-alaoui@uit.ac.ma>
2023-02-04 15:29:08 +0100

    ASSG

--- lib/Screens_code/ScreenHome.dart
+++ lib/Screens_code/ScreenHome.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class TokScreen extends StatelessWidget {
-  const TokScreen({super.key});
+class ScreenHome extends StatelessWidget {
+  const ScreenHome({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
